4fff8fed284ffab0a40a4a150795189c0a0d8074
Avid_practicum <avidm8434@gmail.com>
2023-07-01 17:21:55 +0800

    new file

---
+++ index.js
@@ -0,0 +1,27 @@
+let editbutton=document.querySelector('.profile__info-button')
+let closeditmodalbutton=document.querySelector('.edit-popup__close-button')
+let displayname=document.querySelector('.profile__info-heading')
+let displayabout=document.querySelector('.profile__info-subtitle')
+let saveProfilebutton=document.querySelector('.edit-popup__save-button')
+
+editbutton.addEventListener('click', showEditProfilePopup)
+closeditmodalbutton.addEventListener('click', closeEditProfilePopup)
+saveProfilebutton.addEventListener('click', saveProfile)
+
+  function showEditProfilePopup() {
+    document.querySelector("#editProfilePopup").classList.add('visible')
+  }
+  
+  function closeEditProfilePopup() {
+    document.querySelector("#editProfilePopup").classList.remove('visible')
+  }
+  
+  function saveProfile() {
+    var name = document.getElementById("name").value;
+    var about = document.getElementById("about").value;
+    displayname.textContent=name
+    displayabout.textContent=about
+    
+    closeEditProfilePopup();
+  }
+  
