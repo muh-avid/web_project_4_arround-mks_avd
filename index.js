63bd546d2572910a180777d74d117824a06449c0
Avid_practicum <avidm8434@gmail.com>
2023-07-05 21:42:38 +0800

    deleted & new file

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let editbutton=document.querySelector('.profile__info-button')
-let closeditmodalbutton=document.querySelector('.edit-popup__close-button')
+let closeditmodalbutton=document.querySelector('.popup__close-button')
 let displayname=document.querySelector('.profile__info-heading')
 let displayabout=document.querySelector('.profile__info-subtitle')
-let saveProfilebutton=document.querySelector('.edit-popup__save-button')
+let saveProfilebutton=document.querySelector('.popup__save-button')
 
 editbutton.addEventListener('click', showEditProfilePopup)
 closeditmodalbutton.addEventListener('click', closeEditProfilePopup)
